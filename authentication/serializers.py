ba3c12318156d12a5d933868d251c19cc4294011
suman154 <sumanbhatta735@gmail.com>
2024-08-10 09:16:10 +0545

    User Email Registration

---
+++ authentication/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+# Create your serializers here.
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150, min_length=1)  # Add this line
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True)
+    email = serializers.EmailField(max_length=255, min_length=4)
+    first_name = serializers.CharField(max_length=255, min_length=2)
+    last_name = serializers.CharField(max_length=255, min_length=2)
+
+    class Meta:  # Corrected capitalization
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+
+    def validate(self, attrs):
+        if User.objects.filter(email=attrs['email']).exists():
+            raise serializers.ValidationError({"email": "Email is already in use."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        return user
