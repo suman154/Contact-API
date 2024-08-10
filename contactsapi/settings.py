c95e797a677d467addf354c46d12d3fc37f5b8ac
suman154 <sumanbhatta735@gmail.com>
2024-08-10 08:48:59 +0545

    requirements.txt

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # AppName
+    'authentication',
+    # DRF_APPS
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
